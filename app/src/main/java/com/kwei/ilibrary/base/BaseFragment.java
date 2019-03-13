7b9ce4125eda850e81147c7236a06538ff9707f1
k2archer <k2archer@hotmail.com>
2019-03-13 17:03:36 +0800

    添加 Fragment 的封装基类

---
+++ app/src/main/java/com/kwei/ilibrary/base/BaseFragment.java
@@ -0,0 +1,24 @@
+package com.kwei.ilibrary.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseFragment extends Fragment {
+    private View mRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        mRootView = inflater.inflate(bindLayout(), container, false);
+
+        return mRootView;
+    }
+
+    public abstract int bindLayout();
+    public abstract void onCreateView();
+
+}
