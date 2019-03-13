c021c0fdb0ac19aa3563cf70694dfffc75340f75
k2archer <k2archer@hotmail.com>
2019-03-13 17:06:02 +0800

    添加选项卡的 Fragment 管理实现，及首页的 Fragment 实现

---
+++ app/src/main/java/com/kwei/ilibrary/HomeFragment.java
@@ -0,0 +1,15 @@
+package com.kwei.ilibrary;
+
+import com.kwei.ilibrary.base.BaseFragment;
+
+public class HomeFragment extends BaseFragment {
+
+    @Override
+    public int bindLayout() {
+        return R.layout.fragment_home;
+    }
+
+    @Override
+    public void onCreateView() {
+    }
+}
--- app/src/main/java/com/kwei/ilibrary/MainActivity.java
+++ app/src/main/java/com/kwei/ilibrary/MainActivity.java
@@ -1,13 +1,66 @@
 package com.kwei.ilibrary;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.kwei.ilibrary.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    public void setListener() {
+        $(R.id.menu_home_bt).setOnClickListener(this);
+        $(R.id.menu_home_bt).performClick();
+
+    }
+
+    FragmentManager mManager = getSupportFragmentManager();
+    private String mCurrentFragmentTag;
+
+    private void initFragment() {
+        FragmentTransaction transaction = mManager.beginTransaction();
+        transaction.add(R.id.frame_content, new MeFragment(), String.valueOf(R.id.menu_me_bt));
+        mCurrentFragmentTag = String.valueOf(R.id.menu_home_bt);
+
+        transaction.commit();
+        mManager.executePendingTransactions();
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void widgetClick(View v) {
+        switch (v.getId()) {
+            case R.id.menu_home_bt:
+                Fragment fragment = mManager.findFragmentByTag(String.valueOf(v.getId()));
+                if (fragment != null && !mCurrentFragmentTag.equals(fragment.getTag())) {
+                    Fragment currentFragment = mManager.findFragmentByTag(mCurrentFragmentTag);
+                    FragmentTransaction transaction = mManager.beginTransaction();
+                    if (currentFragment != null) transaction.hide(currentFragment);
+                    transaction.show(fragment).commit();
+                    mManager.executePendingTransactions();
+                    mCurrentFragmentTag = fragment.getTag();
+                }
+                break;
+        }
+
+    }
+
+    @Override
+    public void initView(View view) {
+        initFragment();
+
+    }
+
+    @Override
+    public View bindView() {
+        return null;
+    }
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_main;
     }
 }
