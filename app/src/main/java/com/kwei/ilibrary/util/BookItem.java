6c0f3d8074ab03b9e1d7ae18860631772e6ed41b
k2archer <k2archer@hotmail.com>
2019-03-22 17:17:16 +0800

    添加一个书籍信息显示 Item 的封装类

---
+++ app/src/main/java/com/kwei/ilibrary/util/BookItem.java
@@ -0,0 +1,10 @@
+package com.kwei.ilibrary.util;
+
+public class BookItem {
+    public String name;
+    public String cover;
+    public BookItem(String name, String cover) {
+        this.name = name;
+        this.cover = cover;
+    }
+}
